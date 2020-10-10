47cce296e772f5503b3085909fca38dd72a5e9f1
Maty Siman <Maty.Siman@checkmarx.com>
2020-10-10 03:57:21 +0300

    Show list of added items

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
+//jshint esversion:6
+
 const express = require ("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+global.tasks = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/", function(req,res){
+  tasks.push(req.body.newItem);
+  res.redirect("/");
+
+
+})
 app.get("/", function(req, res){
-  res.send("Hello");
-}
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+/*  if (currentDay === 6 || currentDay === 0)
+  {
+    day = "weekend";
+  }
+  else {
+    day = "weekday"
+  }*/
+  day = days[currentDay];
+  res.render("list", {kindOfDay: day, tasksToPresent: tasks});
+})
 
 app.listen(3000, function(){
   console.log("server started on port 3000");
