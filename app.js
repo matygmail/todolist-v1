d8eba3ca3ab77f02cfdc9938f821351c615b8192
Maty Siman <Maty.Siman@checkmarx.com>
2020-10-10 22:47:00 +0300

    before *

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const bodyParser = require("body-parser");
 
 const app = express();
 global.tasks = [];
-
+let workItems = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static("public"));
 app.post("/", function(req,res){
   tasks.push(req.body.newItem);
   res.redirect("/");
 
 
 })
-app.get("/", function(req, res){
+app.get("*", function(req, res){
+
   var today = new Date();
   var currentDay = today.getDay();
   var day = "";
@@ -29,8 +30,14 @@ app.get("/", function(req, res){
     day = "weekday"
   }*/
   day = days[currentDay];
-  res.render("list", {kindOfDay: day, tasksToPresent: tasks});
+  res.render("list", {listTitle: day, tasksToPresent: tasks});
 })
+
+app.get("/work", function(req, res){
+  console.log(req);
+  res.render("list", {listTitle:"Work List", tasksToPresent: workItems})
+})
+
 
 app.listen(3000, function(){
   console.log("server started on port 3000");
